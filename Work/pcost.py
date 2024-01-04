8846c1e8f0ff4c5876157bfe82435dbaea90606a
kepler <kepler99825@gmail.com>
2024-01-04 23:14:05 +0800

    finish exercise 2.15 and fix exercises 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,20 +3,20 @@
 # Exercise 1.27
 
 import sys
+import csv
 
 
 def portfolio_cost(filename):
     cost = 0.0
 
     with open(filename, "rt") as f:
+        rows = csv.reader(f)
         headers = next(f)
-        try:
-            for line in f:
-                row = line.split(",")
-                cost = cost + int(row[1]) * float(row[2])
-        except ValueError:
-            print("Missing data!")
-
+        for rowno, row in enumerate(rows, start=1):
+            try:
+                cost += int(row[1]) * float(row[2])
+            except ValueError:
+                print(f"Row {rowno}: Bad row: {row}")
     return cost
 
 
