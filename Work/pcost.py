c324be4819c3bc569967b9e969e80bb6588802f6
kepler <kepler99825@gmail.com>
2024-01-04 16:13:25 +0800

    finish exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,18 +2,28 @@
 #
 # Exercise 1.27
 
+import sys
+
 
 def portfolio_cost(filename):
     cost = 0.0
 
     with open(filename, "rt") as f:
         headers = next(f)
-        for line in f:
-            row = line.split(",")
-            cost = cost + int(row[1]) * float(row[2])
+        try:
+            for line in f:
+                row = line.split(",")
+                cost = cost + int(row[1]) * float(row[2])
+        except ValueError:
+            print("Missing data!")
 
     return cost
 
 
-cost = portfolio_cost("Data/portfolio.csv")
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+
+cost = portfolio_cost(filename)
 print("Total cost:", cost)
