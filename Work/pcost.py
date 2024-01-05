51df9c98fad5bfcd9bddc0b64502357a0ba5d254
kepler <kepler99825@gmail.com>
2024-01-05 14:47:45 +0800

    finish exercise 2.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,14 +7,17 @@ import csv
 
 
 def portfolio_cost(filename):
-    cost = 0.0
+    total_cost = 0.0
 
     with open(filename, "rt") as f:
         rows = csv.reader(f)
-        headers = next(f)
+        headers = next(rows)
         for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                cost += int(row[1]) * float(row[2])
+                nshares = int(record["shares"])
+                price = float(record["price"])
+                total_cost += nshares * price
             except ValueError:
                 print(f"Row {rowno}: Bad row: {row}")
     return cost
--- Work/report.py
+++ Work/report.py
@@ -12,7 +12,12 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
+            record = dict(zip(headers, row))
+            holding = {
+                "name": record["name"],
+                "shares": int(record["shares"]),
+                "price": float(record["price"]),
+            }
             portfolio.append(holding)
 
     return portfolio
