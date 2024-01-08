39f98efc5cdc7bbe558d8fe6d426330e08c1907c
kepler <kepler99825@gmail.com>
2024-01-08 20:07:54 +0800

    finish exercise 4.8

--- Work/report.py
+++ Work/report.py
@@ -3,6 +3,7 @@
 import fileparse
 import sys
 import stock
+import tableformat
 
 
 def read_portfolio(filename):
@@ -39,18 +40,17 @@ def make_report_data(portfolio, prices):
     return rows
 
 
-def print_report(reportdata):
+def print_report(reportdata, formatter):
     """
     Print a nicely formated table from a list of (name, shares, price, change) tuples.
     """
-    headers = ("Name", "Shares", "Price", "Change")
-    print("%10s %10s %10s %10s" % headers)
-    print(("-" * 10 + " ") * len(headers))
-    for row in reportdata:
-        print("%10s %10d %10.2f %10.2f" % row)
+    formatter.headings(["Name", "Shares", "Price", "Change"])
+    for name, shares, price, change in reportdata:
+        rowdata = [name, str(shares), f"{price:0.2f}", f"{change:0.2f}"]
+        formatter.row(rowdata)
 
 
-def portfolio_report(portfoliofile, pricefile):
+def portfolio_report(portfoliofile, pricefile, fmt="txt"):
     """
     Make a stock report given portfolio and price data files.
     """
@@ -62,11 +62,12 @@ def portfolio_report(portfoliofile, pricefile):
     report = make_report_data(portfolio, prices)
 
     # Print it out
-    print_report(report)
+    formatter = tableformat.create_formatter(fmt)
+    print_report(report, formatter)
 
 
 def main(args):
-    portfolio_report(args[1], args[2])
+    portfolio_report(args[1], args[2], args[3])
 
 
 if __name__ == "__main__":
