86fe43603cacba2c793546532970c38f31fcf5e3
kepler <kepler99825@gmail.com>
2024-01-12 16:04:39 +0800

    finish exercise 7.9

--- Work/stock.py
+++ Work/stock.py
@@ -1,8 +1,12 @@
 # stock.py
 
+from typedproperty import String, Integer, Float
+
 
 class Stock:
-    __slots__ = ("name", "_shares", "price")
+    name = String("name")
+    shares = Integer("shares")
+    price = Float("price")
 
     def __init__(self, name, shares, price):
         self.name = name
@@ -18,13 +22,3 @@ class Stock:
 
     def __repr__(self):
         return f"Stock('{self.name}', {self.shares}, {self.price})"
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError("expected an integer")
-        self._shares = value
