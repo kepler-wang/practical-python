4332d9779695d3703dcd4629c70fe8b927973f1c
kepler <kepler99825@gmail.com>
2024-01-14 00:11:17 +0800

    finish all exercises

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Kepler Wang",
+    author_email="kepler99825@gmail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
